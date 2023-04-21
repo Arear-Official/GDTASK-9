f96fd620f2a0954708ac656ef16b8292fdb517ab
Arear <119037037+Arear2004@users.noreply.github.com>
2023-04-21 19:09:13 +0300

    Add files via upload

---
+++ gd9/gd9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace gd9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input text");
+            string s = Console.ReadLine();
+            string result = new string(s.Select(c => (char)(c >= 'a' && c <= 'z' ? (c - 'a' + 13) % 26 + 'a' : c >= 'A' && c <= 'Z' ? (c - 'A' + 13) % 26 + 'A' : c)).ToArray());
+            Console.WriteLine(result);
+        }
+    }
+}
